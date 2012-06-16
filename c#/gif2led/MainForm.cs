eb29381e711b0428ed12d8176aabaeb9582abcf7
Brian Nez <brianez21@gmail.com>
2012-06-16 19:21:37 -0400

    Started work in the preliminary C# client software

---
+++ c#/gif2led/MainForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GIF2RGBMatrix
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BrowseButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StartStopButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
